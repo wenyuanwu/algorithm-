7aca2dcb0f66be204081a81444bc05999ad9d583
Wenyuan Wu <wenyuan.wu2012@gmail.com>
2017-08-29 13:42:59 -0700

    int to str

---
+++ elements_of_programming/strings.py
@@ -0,0 +1,20 @@
+# 6.1
+# interconvert strings and integers
+
+def int_to_string(x):
+	is_negative = False
+	if x < 0: 
+	   is_negative = True
+	   x = -x 
+	str = []
+	while x != 0: 
+	   str.append(chr(ord('0') + x % 10))
+	   x //= 10     
+	return ('-'if is_negative else '') + ''.join(reversed(str))   
+
+y = 12300023452
+print(int_to_string(y))
+print(type(int_to_string(y)))
+
+def string_to_int(str):
+   	
